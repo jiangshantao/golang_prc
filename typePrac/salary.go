c89372d7f97052eed455a9d63e9d294cda46074f
姜善涛 <jiangshantao@bytedance.com>
2018-12-12 13:02:22 +0800

    add salary.go

---
+++ typePrac/salary.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+type SalaryCalculator interface {
+	CalculateSalary() int
+}
+
+type normalEmployee struct{
+	empId int
+	basicPay int
+}
+
+type excellentEmployee struct {
+	emId int
+	basicPay int
+	bonus int
+}
+
+func (n normalEmployee)CalculateSalary() int {
+	return n.basicPay
+}
+
+func (e excellentEmployee)CalculateSalary() int {
+	return e.basicPay + e.bonus
+}
+
+func totalExpense(s []SalaryCalculator) int {
+	expense := 0
+	for _, v := range s {
+		expense += v.CalculateSalary()
+	}
+	fmt.Printf("雇员总开支为: %d$", expense)
+
+	return expense
+}
+
+func main(){
+	jst := excellentEmployee{emId:12345, basicPay:8000, bonus:5000}
+	heshan := normalEmployee{empId:23456, basicPay:8000}
+	longqin := excellentEmployee{emId:34567, basicPay:8000, bonus:3000}
+	employees := []SalaryCalculator{jst, heshan, longqin}
+	totalExpense(employees)
+}
+
+ls
